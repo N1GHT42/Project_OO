e5bc678c4d324f437f90e3e2203d197689a2b684
N1GHT42 <matt.nocera@studenti.unina.it>
2025-04-24 23:09:32 +0200

    Create Host.java

---
+++ src/main/java/model/Host.java
@@ -0,0 +1,10 @@
+public class Host extends Utente{
+    public void AddGuest (String login){
+        //Aggiungere un partecipante
+    }
+    public void RemoveGuest (String login){
+        //rimuovere un partecipante
+    }
+
+
+}
